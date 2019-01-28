d8d54418af068cb24240b32aeeff12f898a48150
vutivi <201141790@student.uj.ac.za>
2019-01-28 15:00:25 +0200

    chore: edit, list and destroy actions

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,9 @@
 class ArticlesController < ApplicationController
 
+  def index
+    @articles = Article.all
+  end
+
   def new
     @article = Article.new
   end
@@ -8,13 +12,38 @@ class ArticlesController < ApplicationController
     @article = Article.find(params[:id])
   end
 
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def destroy
+    @article = Article.find(params[:id])
+    if @article.destroy
+      flash[:notice] = "Article was succesfully destroyed."
+      redirect_to articles_path
+    else
+      flash[:error] = "Article cannot be destroyed."
+    end
+
+  end
+
   def create
     @article = Article.new(article_params)
-    if @article.save
+    if @article.update
       flash[:notice] = "Article was succesfully created."
       redirect_to article_path(@article)
     else
       render :new
+    end
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    if @article.save
+      flash[:notice] = "Article was succesfully updated."
+      redirect_to article_path(@article)
+    else
+      render :edit
     end
   end
 

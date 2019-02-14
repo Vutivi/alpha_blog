d8363c79b26a7af1f0b0fe91763cb792e1882b16
vutivi <201141790@student.uj.ac.za>
2019-02-14 16:05:59 +0200

    chore: add secure password and attribute to users

--- Gemfile
+++ Gemfile
@@ -31,6 +31,8 @@ gem 'jquery-rails'
 # Use ActiveStorage variant
 # gem 'mini_magick', '~> 4.8'
 
+#use bcrypt for secure password
+gem 'bcrypt', '~> 3.1.7'
 # Use Capistrano for deployment
 # gem 'capistrano-rails', group: :development
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,5 @@
 class User < ActiveRecord::Base
   has_many :articles
-
   before_save {self.email = email.downcase}
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 
@@ -8,5 +7,6 @@ class User < ActiveRecord::Base
             length: { minimum: 3, maximum: 25 }
   validates :email, presence: true, uniqueness: { case_sensitive: false },
             length: { minimum:10, maximum: 105 }, format: { with: VALID_EMAIL_REGEX }
-  validates :user_id, presence: true
+  
+  has_secure_password
 end
